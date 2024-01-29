51ac60493d942d238fb1b172eea3abfc0a9ce88d
Araldi Ulises Garcia Feliz <araldigarcia@hotmail.com>
2024-01-28 20:44:32 -0400

    Agregar archivos de proyecto.

---
+++ programa_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace programa_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+            Console.WriteLine("Vamos a encontrar el perimetro de un cuadrado");
+            Console.WriteLine("por favor ingrese el tamaño del lado de su cuadrado: ");
+			int lado = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine($"El perimetro de tu cuadrado de lado {lado} es { lado * 4 }");
+			}catch (Exception ex)
+			{
+				Console.WriteLine($"Ha ocurrido un error: {ex.Message}");
+			}
+        }
+	}
+}
